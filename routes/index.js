7997debc6bed1224eaa3b1130ebc75cd65115275
小松 健作 <kensaku.komatsu@gmail.com>
2012-01-20 08:44:41 +0900

    append links etc.

--- routes/index.js
+++ routes/index.js
@@ -7,5 +7,5 @@ exports.index = function(req, res){
   res.render('index', { title: 'WebRTC sample' })
 };
 exports.get_user_media = function(req, res){
-  res.render('get_user_media', { title: 'getUserMedia' })
+  res.render('get_user_media', { title: 'WebRTC sample', subtitle: 'getUserMedia' })
 };
