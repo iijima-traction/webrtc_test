cfc10d6422d490a45b5c435b885f5764965a503e
小松 健作 <kensaku.komatsu@gmail.com>
2012-01-19 21:59:22 +0900

    first commit

---
+++ routes/index.js
@@ -0,0 +1,11 @@
+
+/*
+ * GET home page.
+ */
+
+exports.index = function(req, res){
+  res.render('index', { title: 'WebRTC sample' })
+};
+exports.get_user_media = function(req, res){
+  res.render('get_user_media', { title: 'getUserMedia' })
+};
